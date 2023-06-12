7cc8290e0e7aaa51531ac25a8e7df1c69b370930
swapnil nyayade <swapnilnyayade040@gmail.com>
2023-06-12 11:06:12 +0530

    Liquibase Demo

---
+++ Liquibase/src/main/java/com/swap/liquibase/Liquibase/App.java
@@ -0,0 +1,13 @@
+package com.swap.liquibase.Liquibase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
